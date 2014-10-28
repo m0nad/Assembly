42cb99e7fce3bc1f14be0f63450ae686a5cda5ce
Victor N. Ramos Mello <victornrm@gmail.com>
2014-10-28 19:11:09 -0200

    commiting some old codes...

---
+++ polymorphic_decoder_sub.s
@@ -0,0 +1,20 @@
+# polimorphic shellcode decoder - m0nad
+_start:
+  jmp encoded
+
+decoder:
+  pop %esi
+  push $0x25 # $shellcode_size
+  pop %ecx
+
+decoder_loop:
+  subl $0x5,(%esi,%ecx,1) # shellcode is enconded + 0x5
+  loop decoder_loop
+  jmp shellcode
+
+encoded:
+  call decoder
+
+shellcode:
+# shellcode encoded + 0x5 here
+
